306c7d0cde271a8b7316d5643a1bacd795232d3e
Ahmed Mansour <96149262+amansour13@users.noreply.github.com>
2023-12-29 15:44:57 +0200

    add static class for database
    accessed in the runtime in backend layer

---
+++ src/util/Database.java
@@ -0,0 +1,25 @@
+package src.util;
+
+import src.model.ComponentOrder;
+import src.model.Product;
+import src.model.User;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Queue;
+
+import javax.management.Notification;
+
+public class Database{
+
+    public static Map<String, User> users = new HashMap<String, User>();
+
+    public static Map<String, Product> products = new HashMap<String, Product>();
+    
+    public static Map<Integer, ComponentOrder> orders = new HashMap<Integer, ComponentOrder>();
+
+    public static Statistics stats;
+
+    public static Queue<Notification> notificationsQueue;
+      
+}
