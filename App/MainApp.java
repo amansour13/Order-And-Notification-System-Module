15c237de5b734d58025814572e2d890893487442
Ayman17 <77356245+Ayman17@users.noreply.github.com>
2023-12-29 09:50:07 +0200

    Main UI update

--- App/MainApp.java
+++ App/MainApp.java
@@ -1,5 +1,30 @@
 package App;
 
+import App.AccountManager.*;
+
 public class MainApp {
+
+    private IAccountManager accountManager;
+
+    // Account Manager functions
+    private void register() {
+        accountManager.register();
+    }
+
+    private void login() {
+        accountManager.login();
+    }
+
+    private void addBalance() {
+        // TODO: dummy parameter
+        accountManager.addBalance(5);
+    }
     
+    // 
+    public void run() {
+        this.accountManager = new AccountManager();
+        register();
+        login();
+        addBalance();
+    }
 }
