8d17422cb89b018f578f864126438e4e6de2fca5
Ayman17 <77356245+Ayman17@users.noreply.github.com>
2023-12-29 09:49:16 +0200

    add Notification packages

--- App/Notification.java
+++ App/Notification.java
@@ -1,5 +1,8 @@
 package App;
 
+import App.Channels.ChannelStrategy;
+import App.Messages.MessageTemplate;
+
 public class Notification {
     private ChannelStrategy channel;
     private MessageTemplate message;
