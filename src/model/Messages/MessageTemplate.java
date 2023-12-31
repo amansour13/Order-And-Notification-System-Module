a692c8d25c8e9ade4fb41ce42bd297fcd71d989d
Abdalazez-Sabry <abdalazezsabry00@gmail.com>
2023-12-31 16:31:28 +0200

    Update MessageTemplate.java

--- src/model/Messages/MessageTemplate.java
+++ src/model/Messages/MessageTemplate.java
@@ -9,13 +9,6 @@ public abstract class MessageTemplate {
     
     abstract public void createMessage(ComponentOrder order, User user);
     
-    public String getSubject() {
-        return subject;
-    }
-
-    public void setSubject(String subject) {
-        this.subject = subject;
-    }
 
     public String getContent() {
         return content;
